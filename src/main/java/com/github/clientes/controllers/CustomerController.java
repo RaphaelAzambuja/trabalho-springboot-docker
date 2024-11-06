14e071966542176a2da6e708854f24f1180579f2
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-11-06 13:23:12 -0300

    feat: adicionado updateCustomer

--- src/main/java/com/github/clientes/controllers/CustomerController.java
+++ src/main/java/com/github/clientes/controllers/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.clientes.dto.CreateCustomerDTO;
+import com.github.clientes.dto.UpdateCustomerDTO;
 import com.github.clientes.entities.CustomerEntity;
 import com.github.clientes.services.CustomerService;
 
@@ -53,6 +55,21 @@ public class CustomerController {
     public ResponseEntity<CustomerEntity> createCustomer(@Valid @RequestBody CreateCustomerDTO createCustomerDTO) {
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(createCustomerDTO));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PatchMapping("/{externalUuid}")
+    public ResponseEntity<CustomerEntity> updateCustomer(@PathVariable UUID externalUuid, @Valid @RequestBody UpdateCustomerDTO updateCustomerDTO) {
+        try {
+            CustomerEntity updatedCustomer = customerService.updateCustomer(externalUuid, updateCustomerDTO);
+
+            if (updatedCustomer == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+
+            return ResponseEntity.status(HttpStatus.OK).body(updatedCustomer);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
