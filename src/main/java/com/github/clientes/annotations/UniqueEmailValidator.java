07bda6848ffb1efbc8588347520c18b9745be478
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-11-07 13:06:12 -0300

    fix: ajustado uniqueEmailValidator a fim de validar email de usuario já existente. caso o email existente no banco seja do proprio usuario ele vai pular a validação

--- src/main/java/com/github/clientes/annotations/UniqueEmailValidator.java
+++ src/main/java/com/github/clientes/annotations/UniqueEmailValidator.java
@@ -1,21 +1,38 @@
 package com.github.clientes.annotations;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.clientes.repositories.CustomerRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
     @Autowired
     private CustomerRepository customerRepository;
+    
+    @Autowired
+    private HttpServletRequest request;
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext context) {
-        if (email == null) {
-            return true;
+        String externalUuid = request.getPathInfo();
+
+        if (externalUuid != null && !externalUuid.trim().isEmpty()) {
+            UUID uuid = UUID.fromString(externalUuid);
+            
+            if (uuid != null) {
+                var existingCustomer = customerRepository.findByExternalUuid(uuid);
+                
+                if (existingCustomer != null && existingCustomer.getEmail().equals(email)) {
+                    return true;
+                }
+            }
         }
+
         return !customerRepository.existsByEmail(email);
     }
 }
