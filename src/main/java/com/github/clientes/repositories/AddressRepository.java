9b3f8bc5256c88ba997c7dca1a87146703906458
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-11 13:08:04 -0300

    Feat: Implementação regras de negócio para que seja permitido apenas 5 endereços por cliente.

--- src/main/java/com/github/clientes/repositories/AddressRepository.java
+++ src/main/java/com/github/clientes/repositories/AddressRepository.java
@@ -2,8 +2,12 @@ package com.github.clientes.repositories;
 
 import com.github.clientes.entities.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +15,18 @@ public interface AddressRepository extends JpaRepository <AddressEntity, Long> {
 
     AddressEntity findByExternalUuid(UUID externalUuid);
 
+    @Query("SELECT c.id AS clienteId, COUNT(DISTINCT CONCAT(a.rua, a.bairro, a.cidade, a.uf)) AS totalCadastros " +
+            "FROM AddressEntity a " +
+            "INNER JOIN CustomerEntity c ON a.customer.id = c.id " +
+            "GROUP BY c.id " +
+            "ORDER BY totalCadastros DESC")
+    List<Object[]> countDistinctAddressesByCustomerId();
+
+    @Modifying
+    @Transactional
+    void deleteByExternalUuid(UUID externalUuid);
 }
+
+
+
+
--- src/main/java/com/github/clientes/services/AddressService.java
+++ src/main/java/com/github/clientes/services/AddressService.java
@@ -4,18 +4,25 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
+import com.github.clientes.enterprise.AddressLimitExceededException;
+import com.github.clientes.entities.CustomerEntity;
+import com.github.clientes.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.clientes.dto.CreateAddressDTO;
 import com.github.clientes.entities.AddressEntity;
 import com.github.clientes.repositories.AddressRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AddressService {
 
 @Autowired
 AddressRepository addressRepository;
+
+@Autowired
+CustomerRepository customerRepository;
 
 
     public List<AddressEntity> findAllAddress() {
@@ -43,15 +50,53 @@ AddressRepository addressRepository;
     }
 
 
+    @Transactional
     public AddressEntity createAddress(CreateAddressDTO addressDTO) {
+
+        CustomerEntity customer = customerRepository.findById(addressDTO.customerId())
+                .orElseThrow(() -> new NoSuchElementException("Cliente não encontrado"));
+
+        List<Object[]> addressStats = addressRepository.countDistinctAddressesByCustomerId();
+
+        long totalDistinctAddresses = addressStats.stream()
+                .filter(record -> ((Long) record[0]).equals(customer.getId()))
+                .mapToLong(record -> (Long) record[1])
+                .findFirst()
+                .orElse(0);
+
+        if (totalDistinctAddresses >= 5) {
+            throw new AddressLimitExceededException("Limite de 5 endereços distintos excedido.");
+        }
+
         AddressEntity newAddress = new AddressEntity();
         newAddress.setRua(addressDTO.rua());
         newAddress.setBairro(addressDTO.bairro());
         newAddress.setCidade(addressDTO.cidade());
         newAddress.setUf(addressDTO.uf());
 
+        newAddress.setCustomer(customer);
+
         return addressRepository.save(newAddress);
     }
 
 
+    @jakarta.transaction.Transactional
+    public boolean deleteAddress(String externalUuid) {
+        try {
+            UUID uuid = UUID.fromString(externalUuid);
+            AddressEntity address = addressRepository.findByExternalUuid(uuid);
+
+            if (address == null) {
+                return false;
+            }
+
+            addressRepository.deleteByExternalUuid(uuid);
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao tentar excluir o registro de endereço");
+        }
+    }
+
 }
+
+
