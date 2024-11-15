075cc78f33122711e2bc2b3530dd038d99253d60
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-15 14:31:42 -0300

    feat: Criação das models e dos DTOS necessários

---
+++ src/main/java/com/github/clientes/entities/user/User.java
@@ -0,0 +1,69 @@
+package com.github.clientes.entities.user;
+
+import com.github.clientes.entities.EntityId;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends EntityId implements UserDetails {
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "senha_usuario", nullable = false)
+    private String senha;
+
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
+
+
+
+
