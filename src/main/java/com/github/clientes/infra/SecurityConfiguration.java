027e97f1aabed63029437dcfcb2100fa4ed6fe43
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-16 14:15:03 -0300

    Fix: Alterado o nome da rota de registro para /register na SecurityConfiguration

--- src/main/java/com/github/clientes/infra/SecurityConfiguration.java
+++ src/main/java/com/github/clientes/infra/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/registrar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/addresses").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/customers").hasRole("ADMIN")
                         .anyRequest().authenticated()
